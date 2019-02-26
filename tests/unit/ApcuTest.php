c9bf6ccf42d56294139c9846ee50360468086e5d
Fajrul Akbar Zuhdi <fajrulaz@gmail.com>
2019-02-27 00:04:10 +0700

    rename test

---
+++ tests/unit/ApcuTest.php
@@ -0,0 +1,50 @@
+<?php 
+
+class ApcuTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected $myCache;
+
+    protected function _before()
+    {
+        $adapter = new Viloveul\Cache\ApcuAdapter();
+        $this->myCache = new Viloveul\Cache\Cache($adapter);
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testAdapterInstance()
+    {
+        $this->tester->assertInstanceOf(
+            Viloveul\Cache\Contracts\Adapter::class,
+            $this->myCache->getAdapter()
+        );
+    }
+
+    public function testSetValue()
+    {
+        $this->myCache->set('foo', 'bar');
+        $this->tester->assertTrue(
+            $this->myCache->has('foo')
+        );
+    }
+
+    public function testGetValue()
+    {
+        $this->myCache->set('foo', 'bar');
+        $this->tester->assertEquals('bar', $this->myCache->get('foo'));
+    }
+
+    public function testDelete()
+    {
+        $this->myCache->delete('foo');
+        $this->tester->assertFalse($this->myCache->has('foo'));
+    }
+}
